fb84fcb1dbdf953b0f8b53231470d4985d10e759
mark <454085441@qq.com>
2017-03-18 17:38:43 +0800

    更换新的数据库

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -3,7 +3,7 @@ return array(
 	//'配置项'=>'配置值'
     'DB_TYPE' => 'mysql',
     'DB_HOST'               =>  'localhost',
-    'DB_NAME'               =>  'chukui_base',
+    'DB_NAME'               =>  'chukui_blog',
     'DB_USER'               =>  'root',
     'DB_PWD'                =>  'chukui',
 );
