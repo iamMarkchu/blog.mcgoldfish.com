55eab414fffbd96712bb3e56a3a536ecd8f5208f
mark <454085441@qq.com>
2017-03-19 13:31:36 +0800

    home article 页面

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -2,4 +2,9 @@
 return array(
 	//'配置项'=>'配置值'
     'URL_MODEL' => '2',
+    'URL_ROUTER_ON' => true,
+    'URL_ROUTE_RULES' => [
+        'article/:id' => 'Article/index',
+        'category/:id' => 'Category/index',
+    ],
 );
--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -1,16 +1,12 @@
 <?php
 namespace Home\Controller;
 use Think\Controller;
-class IndexController extends Controller {
+class IndexController extends CommonController {
     public function index(){
         //推荐文章
         $article = D('article');
         $article_list = $article->getRecommandArticle();
         $this->assign('article_list', $article_list);
-        //标签
-        $tag = D('tag');
-        $tag_list = $tag->order('display_order')->select();
-        $this->assign('tag_list', $tag_list);
-        $this->display('front/homepage');
+        $this->display('Front/homepage');
     }
 }
